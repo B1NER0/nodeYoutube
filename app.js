3870f270643807724a359c893747d61112ee68a0
B1NER0 <degenaarp@gmail.com>
2020-09-08 16:20:19 +0200

    testing 502

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
     res.send("Welcome to the hompage baby");
 });
 
-const port = process.env.port || 3000;
+const port = process.env.port || 8000;
 app.listen(port, () => {
     console.log("WAZAPPP");
 });
